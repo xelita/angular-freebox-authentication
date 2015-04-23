7ddbc3bb21b1734fa85181cd4e03b730065fb785
xelita <admin@xelita.com>
2015-04-23 23:00:58 +0200

    Unit tests on requestAuthorization

--- test/freeboxAuthModuleSpec.js
+++ test/freeboxAuthModuleSpec.js
@@ -32,4 +32,50 @@ describe("fbAuthModule Tests Suite", function () {
             expect(fbAuthConstants.tracking.waitingTimeMillis).toBe(5000);
         });
     });
+
+    // fbAuthService
+
+
+    describe("fbAuthService Tests", function () {
+
+        var fbAuthService;
+        var fbAuthConstants;
+
+        var fbCommonService;
+
+        var $http;
+        var $httpBackend;
+
+        beforeEach(function () {
+            module('fbAuthModule');
+            inject(function (_fbAuthService_, _fbAuthConstants_, _fbCommonService_, _$http_, _$httpBackend_) {
+                fbAuthService = _fbAuthService_;
+                fbAuthConstants = _fbAuthConstants_;
+                fbCommonService = _fbCommonService_;
+                $http = _$http_;
+                $httpBackend = _$httpBackend_;
+            });
+        });
+
+        it("requestAuthorization", function () {
+            var expectedApiVersion = {
+                "uid": "23b86ec8091013d668829fe12791fdab",
+                "device_name": "Freebox Server",
+                "api_version": "3.0",
+                "api_base_url": "/api/",
+                "device_type": "FreeboxServer1,1"
+            };
+            var expectedApiCallResponse = {
+                "status": "OK"
+            };
+
+            $httpBackend.expectGET('http://mafreebox.freebox.fr/api_version').respond(expectedApiVersion);
+            $httpBackend.expectPOST('http://mafreebox.freebox.fr/api/v3/login/authorize', {token: ''}).respond(expectedApiCallResponse);
+            var promise = fbAuthService.requestAuthorization({token: ''});
+            promise.then(function (response) {
+                expect(response.data.status).toBe(expectedApiCallResponse.status);
+            });
+            $httpBackend.flush();
+        });
+    });
 });
